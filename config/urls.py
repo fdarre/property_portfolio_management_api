961ba7dbabb99212ec2478524b1b328b353d3e29
Frédéric Darré <frederic.darre@outlook.com>
2020-06-10 15:36:33 +0200

    Add django-debug-toolbar to urls module

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,16 @@
 """
-property_portfolio_management_api URL Configuration
+django_property_management_api URL Configuration.
+The `urlpatterns` list routes URLs to views.
 """
-
 from django.contrib import admin
-from django.urls import path
+from django.conf import settings
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [path("__debug__/", include(debug_toolbar.urls)),] + urlpatterns
