d3ab51b3b0e9293d01a35314dfc71f7c99c42caf
Frédéric Darré <frederic.darre@outlook.com>
2020-07-28 09:03:22 +0200

    Add and remove fields to properties app models
    The "currency" field is deleted form both "Expense" and "Income" models.
    The currency will be set in the project settings.

--- properties/models.py
+++ properties/models.py
@@ -4,17 +4,21 @@ from django.db import models
 
 class Building(models.Model):
     address = AddressField()
+    number_of_units = models.IntegerField(max_length=20, null=True, blank=True)
+    number_of_floors = models.IntegerField(max_length=20)
+    surface_area = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
     ownerName = models.CharField(max_length=30, null=True, blank=True)
-    is_in_protected_heritage_area = models.BooleanField()
     construction_date = models.DateField(null=True, blank=True)
     acquisition_date = models.DateField(null=True, blank=True)
     acquisition_price = models.DecimalField(
-        max_digits=8, decimal_places=2, null=True, blank=True
+        max_digits=9, decimal_places=2, null=True, blank=True
     )
     current_price = models.DecimalField(
-        max_digits=8, decimal_places=2, null=True, blank=True
+        max_digits=9, decimal_places=2, null=True, blank=True
     )
     is_co_ownership = models.BooleanField()
+
+    is_in_protected_heritage_area = models.BooleanField()
 
     def __str__(self):
         return f"{self.address}"
@@ -25,18 +29,20 @@ class Unit(models.Model):
         LEFT = "le", "Left"
         RIGHT = "ri", "Right"
         CENTRE = "ce", "Center"
+        CENTRE_LEFT = "cl", "Center Left"
+        CENTRE_RIGHT = "cr", "Center Right"
 
     class Properties_type(models.TextChoices):
         APPARTMENT = "ap", "Appartment"
         HOUSE = "ho", "House"
 
+    property_type = models.CharField(max_length=2, choices=Properties_type.choices)
+    floor = models.IntegerField(null=True, blank=True)
     position = models.CharField(
         max_length=2, choices=Positions.choices, null=True, blank=True
     )
-    property_type = models.CharField(max_length=2, choices=Properties_type.choices)
-
-    floor = models.IntegerField(null=True, blank=True)
-    is_occupied = models.BooleanField()
+    surface_area = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    #@todo: add uploadable picture
     building = models.ForeignKey(Building, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -56,52 +62,30 @@ class Expense(models.Model):
         CONDO_FEES = "cf", "Condo Fees"
         OVERPAYMENT_REFUND = "or", "Overpayment Refund"
 
-    class Currencies(models.TextChoices):
-        EUR = "eu", "EUR"
-        USD = "us", "USD"
-        JPY = "jp", "JPY"
-        GBP = "gb", "GBP"
-        AUD = "au", "AUD"
-        CAD = "ca", "CAD"
-        CHF = "ch", "CHF"
-        CNY = "cn", "CNY"
-
     category = models.CharField(max_length=2, choices=Categories.choices)
-    currency = models.CharField(max_length=2, choices=Currencies.choices)
     payment_date = models.DateField()
-    amount = models.DecimalField(max_digits=6, decimal_places=2)
-    observation = models.CharField(max_length=100, null=True, blank=True)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    observations = models.CharField(max_length=100, null=True, blank=True)
     unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
     building = models.ForeignKey(Building, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.category}, {self.amount} {self.currency}"
+        return f"{self.category}, {self.amount}" #@todo: Display the chosen currency
 
 
 class Income(models.Model):
     class Categories(models.TextChoices):
-        # Insurance Compensations will be managed from another app
+        INSURANCE_COMPENSATION = "ic", "Insurance Compensation"
         RENT = "re", "Rent"
         HOUSING_ALLOWANCE = "ha", "Housing Allowance"
         OTHER_PAYMENT = "ot", "Other Payment"
 
-    class Currencies(models.TextChoices):
-        EUR = "eu", "EUR"
-        USD = "us", "USD"
-        JPY = "jp", "JPY"
-        GBP = "gb", "GBP"
-        AUD = "au", "AUD"
-        CAD = "ca", "CAD"
-        CHF = "ch", "CHF"
-        CNY = "cn", "CNY"
-
     category = models.CharField(max_length=2, choices=Categories.choices)
-    currency = models.CharField(max_length=2, choices=Currencies.choices)
     payment_date = models.DateField()
-    amount = models.DecimalField(max_digits=6, decimal_places=2)
-    observation = models.CharField(max_length=100, null=True, blank=True)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    observations = models.CharField(max_length=100, null=True, blank=True)
     unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
     building = models.ForeignKey(Building, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.category}, {self.amount} {self.currency}"
+        return f"{self.category}, {self.amount}" #@todo: Display the chosen currency
