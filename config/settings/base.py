d87939d33fb0f4a5bcb99342374679f4c8ca04fc
Frédéric Darré <frederic.darre@outlook.com>
2020-08-02 08:34:16 +0200

    Add pagination and set global permission policy

--- config/settings/base.py
+++ config/settings/base.py
@@ -181,6 +181,8 @@ GOOGLE_API_KEY = env("GOOGLE_API_KEY")
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.rest_framework.IsAdminUser'
-    ]
+        'rest_framework.permissions.IsAdminUser'
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
